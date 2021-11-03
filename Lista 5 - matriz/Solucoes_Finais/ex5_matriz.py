87a1ea11598dc7c6911f2e9ddd4c12995af7bf3f
annapss <annpss07@gmail.com>
2021-11-03 10:29:04 -0300

    Usando o while e mudando a forma como é feita a matriz transposta

--- Lista 5 - matriz/Solucoes_Finais/ex5_matriz.py
+++ Lista 5 - matriz/Solucoes_Finais/ex5_matriz.py
@@ -10,7 +10,8 @@ onde AT[i][j] = A[j][i]
 
 ordem = int(input('Para uma matriz quadrada A, informe sua ordem:  '))
 
-if ordem < 1 or ordem > 100:
+#Ao invés de usar o if e o while, pode usar só o while
+"""if ordem < 1 or ordem > 100:
     cont = 1
     while cont == 1:
         print('Valor inválido')
@@ -18,37 +19,41 @@ if ordem < 1 or ordem > 100:
         cont = 0
         if ordem < 1 or ordem > 100:
             cont = 1
+"""
+while(ordem < 1 or ordem > 100):
+    print("Valor invalido")
+    ordem = int(input('Para uma matriz quadrada A, informe sua ordem:  '))
 
-A = []
+#não é bom usar nome de variavel com letra maiúscula pois geralmente usamos letra maiúscula quando é um const
+matrizA = []
 for i in range(ordem):
     linha = []
     for j in range(ordem):
         linha.append(int(input('Digite um valor: ')))
-    A.append(linha)
+    matrizA.append(linha)
 
 print('\nMatriz original: A')
 for i in range(ordem):
-    print(A[i])
+    print(matrizA[i])
 
 #formação da matriz transposta:
-matriz = []
+matrizT = []
 for i in range(ordem):
     linha = []
     for j in range(ordem):
         linha.append(0)
-    matriz.append(linha)
+    matrizT.append(linha)
 
 for i in range(ordem):
     for j in range(ordem):
-        if i == j:
-            matriz[i][j] = A[i][j]
+        #Esse if e elif funciona mas é mais fácil só fazer matriz[i][j] = matrizA[j][i] como ele colocou
+        #no enunciado
+        matrizT[i][j] = matrizA[j][i]
+        """if i == j:
+            matrizT[i][j] = matrizA[i][j]
         elif i != j:
-            matriz[j][i] = A[i][j]
+            matrizT[j][i] = matrizA[i][j]"""
 
 print('\nMatriz transposta: At')
 for i in range(ordem):
-    print(matriz[i])
-
-
-
-
+    print(matrizT[i])
