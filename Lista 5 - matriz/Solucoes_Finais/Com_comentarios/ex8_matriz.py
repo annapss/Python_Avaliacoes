f82c096e5287e4a7a442bb0f56d86070a41a649c
annapss <annpss07@gmail.com>
2021-11-03 14:40:27 -0300

    Mudança no mediaAlunos de vetor para matriz

--- Lista 5 - matriz/Solucoes_Finais/Com_comentarios/ex8_matriz.py
+++ Lista 5 - matriz/Solucoes_Finais/Com_comentarios/ex8_matriz.py
@@ -14,59 +14,62 @@ maior que a média de sua turma.
 turmas = 2
 alunos = 3
 notas = 2
-M = []
+matrizPrin = []
+medias = [] #criei um vetor medias pra colocar as medias dos alunos de cada turma
 mediaAlunos = []
-TURMA = []
-
+mediasTurmas = []
 for i in range(turmas):  #criação da matriz e cáculo da média de cada aluno
     aux = []
     print(f'\nTurma {i+1}')
+    medias = []
     for j in range(alunos):
         print(f'Aluno {j+1}')
         linha = []
         soma = 0
         for z in range(notas):
-            linha.append(float(input(f'digite sua nota {z+1}: ')))
+            linha.append(float(input(f'Digite sua nota {z+1}: ')))
             soma += linha[z]
-        linha.append(soma/notas)
+        linha.append(soma/notas) #media
         aux.append(linha)
-        mediaAlunos.append(soma/notas)
-
-    M.append(aux)
-
-soma = 0
-j = 0
-
-for i in range(len(mediaAlunos)):  #cálculo da média de cada turma
-    soma += mediaAlunos[i]
+        medias.append(soma/notas)
+    mediaAlunos.append(medias) #depois adiciono em outro vetor pra fazer uma matriz
+    matrizPrin.append(aux)
+#cada linha de mediaAlunos representa as medias dos alunos de uma turma
+#com isso, posso usar o sum()
+for i in range(turmas):  #cálculo da média de cada turma
+    mediasTurmas.append(sum(mediaAlunos[i]) / alunos)
+    """soma += mediaAlunos[i]
     j +=1
     if j == 3:
         TURMA.append(soma/alunos)
         soma = 0
         j = 0
+    """
 
-maior = 0
-grp = 0
-for i in range(turmas):  #verificação de qual turma tem a maior média
+maior = max(mediasTurmas)
+grp = mediasTurmas.index(max(mediasTurmas))
+#Usando o max() e o index(), não precisamos do for
+"""for i in range(turmas):  #verificação de qual turma tem a maior média
     if i == 0:
         maior = TURMA[i]
     else:
         if TURMA[i] > maior:
             maior = TURMA[i]
             grp = i
-
+"""
 print('\nMatriz M:')
-for aux in M:
+for aux in matrizPrin:
     print(aux)
 
 print(f'\nA turma {grp+1} possui a maior média com {maior:.2f}')
 
-z = 0
-for i in range(len(mediaAlunos)):  #verificação de que alunos tiveram média superior a de sua turma
-    if i <= 2:
-        if mediaAlunos[i] > TURMA[0]:
-            print(f'O aluno {i+1} da turma 1 tem a média({mediaAlunos[i]}) como maior que a média de sua turma.')
-    elif i > 2:
-        z +=1
-        if mediaAlunos[i] > TURMA[1]:
-            print(f'O aluno {z} da turma 2 tem a média({mediaAlunos[i]}) como maior que a média de sua turma.')
+#z = 0
+for i in range(turmas):
+    for j in range(alunos):  #verificação de que alunos tiveram média superior a de sua turma
+    ##if i <= 2: #media dos alunos da primeira turma
+        if mediaAlunos[i][j] > mediasTurmas[i]:
+            print(f'O aluno {i+1} da turma 1 tem a média({mediaAlunos[i][j]:.2f}) como maior que a média de sua turma.')
+    ##elif i > 2: #media dos alunos da segunda turma
+        ##z +=1
+        ##if mediaAlunos[i] > TURMA[1]:
+            ##print(f'O aluno {z} da turma 2 tem a média({mediaAlunos[i]}) como maior que a média de sua turma.')
