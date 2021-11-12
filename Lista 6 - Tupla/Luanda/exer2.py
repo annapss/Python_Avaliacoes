cb0763a45818d138a3612014a249f5b2100b9887
luarodri <luandarodrigues331@gmail.com>
2021-11-12 14:51:20 -0300

    2 exercício ainda sem ser feito, mas eu já já faço

---
+++ Lista 6 - Tupla/Luanda/exer2.py
@@ -0,0 +1,10 @@
+'''
+Grupo: Anna Paula Siqueira, Maria Laura Barbosa, Luanda Rodrigues e Ariane Arantes
+Turma: 2BINFO
+
+Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do atleta será determinado 
+pela média dos cinco valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas 
+pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando
+não for informado o nome do atleta.
+
+'''
