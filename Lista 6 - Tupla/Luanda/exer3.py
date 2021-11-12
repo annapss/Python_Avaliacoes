185f7781fb4169ab459de75b82e2d2dc4e798ce0
luarodri <luandarodrigues331@gmail.com>
2021-11-12 15:54:56 -0300

    exercício 3, também sem ser feito

---
+++ Lista 6 - Tupla/Luanda/exer3.py
@@ -0,0 +1,14 @@
+'''
+Grupo: Anna Paula Siqueira, Maria Laura Barbosa, Luanda Rodrigues e Ariane Arantes
+Turma: 2BINFO
+
+#esse aqui é um
+Faça um programa que preencha por leitura um vetor de 10 posições, e conta quantos valores diferentes existem no vetor.
+
+#esse daqui é outro
+Faça um programa que preencha por leitura um vetor
+de 5 posições, e informe a posição em que um valor x
+(lido do teclado) está no vetor. Caso o valor x não seja
+encontrado, o programa deve imprimir "Valor não encontrado".
+
+'''
