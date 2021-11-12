9658cf0f5b83ab9cfe10229bfaac29c1a2df61b6
annapss <annpss07@gmail.com>
2021-11-12 19:57:50 -0300

    Alterações nos exercícios da Luanda

--- Lista 6 - Tupla/Luanda/exer1.py
+++ Lista 6 - Tupla/Luanda/exer1.py
@@ -2,7 +2,8 @@
 Grupo: Anna Paula Siqueira, Maria Laura Barbosa, Luanda Rodrigues e Ariane Arantes
 Turma: 2BINFO
 
-Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. Após isto, calcule a 
-média anual das temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram.
+Faça um programa que receba a temperatura média de cada mês do ano e armazene-as 
+em uma lista. Após isto, calcule a média anual das temperaturas e mostre todas 
+as temperaturas acima da média anual, e em que mês elas ocorreram.
 
 '''
