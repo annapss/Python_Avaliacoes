76001e3cff2e7d1f294d1101e5d077392170e390
annapss <annpss07@gmail.com>
2021-12-17 15:46:57 -0300

    Exercício 2 - Contagem de números

---
+++ Lista 7/exe02.py
@@ -0,0 +1,19 @@
+tam = 5
+vetor = [0] * tam
+def percurso(n, i=0):
+    if(i == n):
+        return False
+    else:
+        vetor[i] = int(input("Digite um número: "))
+        return percurso(tam, i + 1)
+percurso(tam)
+passei = []
+def contagem(n, i=0):
+    if(i == n):
+        return False
+    else:
+        if(vetor[i] not in passei):
+            passei.append(vetor[i])
+            print(f"\nO número {vetor[i]} aparece {vetor.count(vetor[i])} vezes")
+        return contagem(tam, i + 1)
+contagem(tam)
