682fdf52047cbe46ebdd8625b04892ed3a5826f3
annapss <annpss07@gmail.com>
2022-01-28 08:22:17 -0300

    Tentativa de solução para a 6

---
+++ Lista 9 - LSE/Anna Paula/exe06.py
@@ -0,0 +1,113 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.prox = None
+
+class ListaEncadeada:
+    def __init__(self):
+        self.inicio = None
+
+    def inserir(self, valor):
+        aux = No(valor)
+        aux.prox = self.inicio
+        self.inicio = aux
+
+    def criarLSEInversa(self):
+        valor = int(input('Entre com um valor: '))
+        if (valor <= 0):
+            return 0
+        else:
+            self.inserir(valor)
+            self.criarLSEInversa()
+
+    def mostrar(self, i=0, inicio=0):
+        if(i == 0):
+            inicio = self.inicio
+        aux = inicio
+        if(i == 0):
+            print('\nConteúdoda Lista Simplesmente Encadeada:')
+        if (aux == None and i == 0):
+            print('Lista vazia')
+        if (aux != None):
+            print(aux.valor, end=' ')
+            aux = aux.prox
+            inicio = aux
+            self.mostrar(i + 1, inicio)
+        else:
+            return 0
+
+    def tamanho(self, i=0, inicio=0):
+        if(i == 0):
+            inicio = self.inicio
+        aux = inicio
+        if (aux == None):
+            print(f"O tamanho da lista é {i}")
+            return i
+        else:
+            aux = aux.prox
+            inicio = aux
+            self.tamanho(i + 1, inicio)
+    
+    def maior(self, i=0, inicio=0, more=0):
+        if(i == 0):
+            inicio = self.inicio
+        aux = inicio
+        if (aux != None):
+            if(i == 0):
+                more = aux.valor
+            if (aux.valor > more):
+                more = aux.valor
+            aux = aux.prox
+            inicio = aux
+            self.maior(i + 1, inicio, more)
+        else:
+            if(i != 0):
+                print(f'O maior valor é {more}')
+                return more
+            else:
+                return None
+    
+    def menor(self, i=0, inicio=0, less=0):
+        if(i == 0):
+            inicio = self.inicio
+        aux = inicio
+        if (aux != None):
+            if(i == 0):
+                less = aux.valor
+            if (aux.valor < less):
+                less = aux.valor
+            aux = aux.prox
+            inicio = aux
+            self.menor(i + 1, inicio, less)
+        else:
+            if(i != 0):
+                print(f'O menor valor é {less}')
+                return less
+            else: 
+                return None
+
+    def media(self, i = 0, inicio = 0, media = 0):
+        if(i == 0):
+            inicio = self.inicio
+        aux = inicio
+        if (aux == None):
+            if(i != 0):
+                media /= self.tamanho()
+                print(media)
+                return media
+            else:
+                return None
+        else:
+            media += aux.valor
+            aux = aux.prox
+            inicio = aux
+            return self.media(i + 1, inicio, media)
+    
+lista = ListaEncadeada()
+lista.criarLSEInversa()
+lista.mostrar()
+print()
+lista.tamanho()
+lista.maior()
+lista.menor()
+print(lista.media())
