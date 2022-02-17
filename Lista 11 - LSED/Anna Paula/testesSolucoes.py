d592b06d6813e3b4f994279bd8fffedcb70dc36c
annapss <annpss07@gmail.com>
2022-02-16 22:27:48 -0300

    Nome dos integrantes e exercícios

---
+++ Lista 11 - LSED/Anna Paula/testesSolucoes.py
@@ -0,0 +1,14 @@
+"""
+Grupo: Anna Paula Siqueira da Silva, Maria Laura Barbosa Soares, Luanda Rodrigues da Silva e 
+Ariane Arantes dos Santos
+
+Turma: 2BINFO
+
+1. Criar LSEC, LDEC, LSED, LDED, LSECD e LDECD na ordem em que os elementos foram digitados
+2. Criar LSEC, LDEC, LSED, LDED, LSECD e LDECD na ordem inversa em que os elementos foram digitados
+3. Inserir elemento mantendo a lista ordenada (se a lista existir, supor que a mesma está em ordem 
+crescente de valor). LSEC, LDEC, LSED, LDED, LSECD e LDECD.
+4. Criar LSEC, LDEC, LSED, LDED, LSECD e LDECD em ordem crescente de valor
+5. Remover um elemento de uma LSEC, LDEC, LSED, LDED, LSECD e LDECD.
+6. Apagar a lista da mémoriaLSEC, LDEC, LSED, LDED, LSECD e LDECD.
+"""
